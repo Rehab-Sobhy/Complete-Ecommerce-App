1a3309d62e79dbce0f324fe5702b2f9907636c89
Rehab <rehabsobhy184@gmail.com>
2024-04-29 02:46:45 +0300

    creating best seller list viewin home page

--- lib/home_screen/home.dart
+++ lib/home_screen/home.dart
@@ -12,48 +12,97 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List images = [
-    "assets/images/photo_2024-04-26_02-11-05.jpg",
-    "assets/images/photo_2024-04-26_02-11-05.jpg",
-    "assets/images/photo_2024-04-26_02-11-05.jpg",
-    "assets/images/photo_2024-04-26_02-11-05.jpg",
-    "assets/images/photo_2024-04-26_02-11-05.jpg"
+    "assets/images/R.png",
+    "assets/images/R.png",
+    "assets/images/R.png",
+    "assets/images/R.png",
+    "assets/images/R.png",
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            const Text(
-              "Do shopping Now 🔥",
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Text("Categories"),
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 200,
-              child: Expanded(
-                child: ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        padding: EdgeInsets.all(5),
-                        height: 80,
-                        width: 80,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle, image: images[index]),
-                      );
-                    }),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text(
+                "Do shopping Now 🔥",
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 150,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text("Categories",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                ],
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              SizedBox(
+                height: 80,
+                child: Expanded(
+                  child: ListView.builder(
+                      itemCount: images.length,
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemBuilder: (context, int index) {
+                        return Container(
+                            padding: EdgeInsets.all(5),
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: AssetImage(images[index]),
+                                )));
+                      }),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text("Best Seller",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                ],
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              SizedBox(
+                height: 300,
+                child: Expanded(
+                  child: ListView.builder(
+                      itemCount: images.length,
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemBuilder: (context, int index) {
+                        return Container(
+                            margin: EdgeInsets.symmetric(horizontal: 10),
+                            padding: EdgeInsets.all(5),
+                            width: 200,
+                            height: 300,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: AssetImage(
+                                images[index],
+                              ),
+                            )));
+                      }),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
