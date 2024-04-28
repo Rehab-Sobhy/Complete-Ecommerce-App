150f92c2e1abe194cec569448593294c0d8394da
Rehab <rehabsobhy184@gmail.com>
2024-04-29 01:41:08 +0300

    finish splash screen

--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 // ignore: implementation_imports
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_commerce_app/core/utils/styles.dart';
+import 'package:flutter_commerce_app/pageView/presntation/views/on_boarding_page_view.dart';
 
 class Splash_Screen extends StatefulWidget {
   const Splash_Screen({super.key});
@@ -14,21 +16,33 @@ class Splash_Screen extends StatefulWidget {
 // ignore: camel_case_types
 class _Splash_ScreenState extends State<Splash_Screen> {
   @override
+  void initState() {
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => OnBoardingPageView()));
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     // ignore: sized_box_for_whitespace
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      color: mainColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "🅔 🅒🅞🅜🅜🅔🅡🅒🅔 🅐🅟🅟",
-            style: Styles.TextStyle30.copyWith(
-                color: Color.fromRGBO(22, 33, 33, 39)),
-          ),
-        ],
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: secondColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "🅔 🅒🅞🅜🅜🅔🅡🅒🅔 🅐🅟🅟",
+              style: Styles.TextStyle30.copyWith(
+                  color: Color.fromRGBO(0, 0, 0, 0.851)),
+            ),
+          ],
+        ),
       ),
     );
   }
