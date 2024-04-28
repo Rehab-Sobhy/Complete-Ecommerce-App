0f79614489b1a642008cfa729dd7db98c8881c64
Rehab <rehabsobhy184@gmail.com>
2024-04-28 03:08:25 +0300

    edit in register screen

--- lib/auth/register.dart
+++ lib/auth/register.dart
@@ -145,7 +145,7 @@ class RegisterScreen extends StatelessWidget {
                                 Navigator.push(
                                     context,
                                     MaterialPageRoute(
-                                        builder: (context) => ()));
+                                        builder: (context) => Login_Screen()));
                               },
                               child: const Text('Login ',
                                   style: TextStyle(
