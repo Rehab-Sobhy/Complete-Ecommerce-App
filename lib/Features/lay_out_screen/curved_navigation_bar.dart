4ed79707198c8e569c0bc9251b1b81cbec514fee
Rehab <rehabsobhy184@gmail.com>
2024-05-02 21:48:21 +0300

    add screens to bottom navigation bar

--- lib/Features/lay_out_screen/curved_navigation_bar.dart
+++ lib/Features/lay_out_screen/curved_navigation_bar.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_commerce_app/Features/account/presentation/views/account_view.dart';
-import 'package:flutter_commerce_app/Features/lay_out_screen/home.dart';
+import 'package:flutter_commerce_app/Features/cart/cart.dart';
+import 'package:flutter_commerce_app/Features/categories/category.dart';
+import 'package:flutter_commerce_app/Features/favourite/favourite.dart';
+import 'package:flutter_commerce_app/Features/home_screen/home.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,14 +16,21 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List Screens = [
     HomePage(),
-    AccountView(),
+    const Category_Screen(),
+    const FavouriteScreen(),
+    const CartScreen(),
+    const AccountView(),
   ];
   int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-          fixedColor: const Color(0xF7FCF6F8),
+          unselectedFontSize: 20,
+          selectedFontSize: 30,
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.red,
+          fixedColor: const Color.fromARGB(247, 0, 0, 0),
           backgroundColor: Colors.white,
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home)),
