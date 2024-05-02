8e35094faabded41b42c96bb4c9eaa97c09474d7
Rehab <rehabsobhy184@gmail.com>
2024-05-02 21:41:27 +0300

    create favourite screen

---
+++ lib/Features/favourite/favourite.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FavouriteScreen extends StatefulWidget {
+  const FavouriteScreen({super.key});
+
+  @override
+  State<FavouriteScreen> createState() => _FavouriteScreenState();
+}
+
+class _FavouriteScreenState extends State<FavouriteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: double.infinity, color: Colors.amber);
+  }
+}
