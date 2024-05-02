96ceb5b3bc1bd01a55614242c9e62041f6fbe0f1
Rehab <rehabsobhy184@gmail.com>
2024-05-02 21:40:17 +0300

    create category screen

---
+++ lib/Features/categories/category.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Category_Screen extends StatefulWidget {
+  const Category_Screen({super.key});
+
+  @override
+  State<Category_Screen> createState() => _Category_ScreenState();
+}
+
+class _Category_ScreenState extends State<Category_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: double.infinity, color: Colors.amber);
+  }
+}
