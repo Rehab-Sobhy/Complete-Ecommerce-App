e960c72671819033239f89b8e91adbce8b52a141
Rehab <rehabsobhy184@gmail.com>
2024-05-02 16:00:53 +0300

    determine the cubit and the state of block consumer

--- lib/home_screen/home.dart
+++ lib/home_screen/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_commerce_app/lay0ut_cubit/layout_cubit.dart';
+import 'package:flutter_commerce_app/lay0ut_cubit/layout_cubit_states.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,27 +16,34 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final cubit = BlocProvider.of<LayOutCubit>(context);
-    return BlocConsumer(
+    return BlocConsumer<LayOutCubit, LayoutCubitState>(
         builder: ((context, state) {
           return Scaffold(
-            body: ListView(
-              shrinkWrap: true,
-              children: [
-                cubit.banners.isEmpty
-                    ? CircularProgressIndicator()
-                    : SizedBox(
-                        width: double.infinity,
-                        height: 130,
-                        child: PageView.builder(
-                          itemCount: cubit.banners.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Container(
-                              child: Image.network(cubit.banners[index].url!),
-                            );
-                          },
-                        ),
-                      )
-              ],
+            body: Padding(
+              padding: const EdgeInsets.all(12),
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  cubit.banners.isEmpty
+                      ? CircularProgressIndicator()
+                      : SizedBox(
+                          width: double.infinity,
+                          height: 130,
+                          child: PageView.builder(
+                            itemCount: cubit.banners.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                margin: EdgeInsets.all(7),
+                                child: Image.network(
+                                  cubit.banners[index].url!,
+                                  fit: BoxFit.fill,
+                                ),
+                              );
+                            },
+                          ),
+                        )
+                ],
+              ),
             ),
           );
         }),
--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: AccountView(),
+          home: HomePage(),
           debugShowCheckedModeBanner: false,
         ));
   }
