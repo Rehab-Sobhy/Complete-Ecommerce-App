a70de6060c8f9b721fcd1b19f9e1a0955a4aafd4
Rehab <rehabsobhy184@gmail.com>
2024-05-05 19:11:17 +0300

    add new states to lay out cubit states

--- lib/Features/lay_out_screen/lay0ut_cubit/layout_cubit_states.dart
+++ lib/Features/lay_out_screen/lay0ut_cubit/layout_cubit_states.dart
@@ -34,3 +34,47 @@ class FilteringProducts extends LayoutCubitState {}
 class GetFavoritesSuccessState extends LayoutCubitState {}
 
 class FailedToGetFavoritesState extends LayoutCubitState {}
+
+class AddOrRemoveItemFromFavoritesSuccessState extends LayoutCubitState {}
+
+class FailedToAddOrRemoveItemFromFavoritesState extends LayoutCubitState {}
+
+class GetBannersLoadingState extends LayoutCubitState {}
+
+class GetBannersSuccessState extends LayoutCubitState {}
+
+class FailedToGetBannersState extends LayoutCubitState {}
+
+class GetCategoriesSuccessState extends LayoutCubitState {}
+
+class FailedToGetCategoriesState extends LayoutCubitState {}
+
+class GetProductsSuccessState extends LayoutCubitState {}
+
+class FailedToGetProductsState extends LayoutCubitState {}
+
+class FilterProductsSuccessState extends LayoutCubitState {}
+
+class GetCartsSuccessState extends LayoutCubitState {}
+
+class FailedToGetCartsState extends LayoutCubitState {}
+
+class ChangePasswordLoadingState extends LayoutCubitState {}
+
+class ChangePasswordSuccessState extends LayoutCubitState {}
+
+class ChangePasswordWithFailureState extends LayoutCubitState {
+  String error;
+
+  ChangePasswordWithFailureState(this.error);
+}
+
+class UpdateUserDataLoadingState extends LayoutCubitState {}
+
+class UpdateUserDataSuccessState extends LayoutCubitState {}
+
+class UpdateUserDataWithFailureState extends LayoutCubitState {
+  String error;
+
+  UpdateUserDataWithFailureState(this.error);
+}
